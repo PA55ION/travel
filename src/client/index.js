d3c8e170288fd45a5952ba144939d3e45575aa8b
Mai Campbell <jirallc@gmail.com>
2020-04-01 12:20:53 -0700

    style: complete input form on small devices

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,5 @@ import './styles/header.scss';
 import './styles/base.scss';
 import './styles/main.scss';
 import './styles/responsive.scss';
+import './styles/form.scss';
 import background from '../assets/background.jpg'
