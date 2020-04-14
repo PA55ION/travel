331b977679e1cb01cb49e198db097941eca0b3b1
Mai Campbell <jirallc@gmail.com>
2020-04-14 15:20:43 -0700

    change text in text input

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -69,7 +69,7 @@ export function handleSubmit(e) {
             setTimeout(() => {
                 document.getElementById('travel-info').innerHTML = `Your trip to ${cityName} is ${days} days away. It's time to Pack you bag :)`
                 getCity(`${restCountry}${cityName}`)
-            }, 2000)
+            }, 15000)
         // 
     });
 }
