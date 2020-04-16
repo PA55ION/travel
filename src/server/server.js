0155894cc82fd7ff5084e48171635bf0cc88c1c6
Mai Campbell <jirallc@gmail.com>
2020-04-16 09:41:30 -0700

    clean up code

--- src/server/server.js
+++ src/server/server.js
@@ -24,10 +24,7 @@ app.use(bodyParser.json());
 //COMMENT Middle ware
 app.use(cors());
 app.use(express.static('dist'));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+
 console.log(__dirname)
 
 //COMMENT spin up the server
